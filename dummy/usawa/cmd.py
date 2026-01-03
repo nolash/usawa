b9223f1c63c475afb5e855f26b5431ba94cfd682
lash <dev@holbrook.no>
2026-01-03 10:16:40 +0100

    Add docs to XML module

--- dummy/usawa/cmd.py
+++ dummy/usawa/cmd.py
@@ -4,5 +4,3 @@ import rencode
 class Op(enum.IntEnum):
     NOOP = 0
     UPDATE = 1
-
-
--- dummy/usawa/xml.py
+++ dummy/usawa/xml.py
@@ -11,12 +11,18 @@ script_dir = os.path.realpath(os.path.dirname(__file__))
 
 
 def nsmap():
+    """Namespaces to use in ledger XML tree operations.
+    """
     return NAMESPACES
 
 
 def parse(v):
+    """Parse ledger XML tree from XML string.
+    """
     return lxml.etree.fromstring(v)
 
 
 def dump(v):
+    """Generate XML string from ledger XML tree.
+    """
     return lxml.etree.tostring(v)
