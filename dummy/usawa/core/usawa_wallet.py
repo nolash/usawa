3b80287bb113d1fb58ab646cf467a488bc13b102
Carlosokumu <carlosokumu254@gmail.com>
2026-03-14 09:46:52 +0300

    decrypt using secretbox

--- dummy/usawa/core/usawa_wallet.py
+++ dummy/usawa/core/usawa_wallet.py
@@ -1,19 +1,39 @@
-import gnupg
 import logging
-
 from usawa.crypto import DemoWallet
+
+from nacl.secret import SecretBox
+from nacl.pwhash import argon2i
 
 
 logg = logging.getLogger("usawawallet")
 
 
 class UsawaWallet(DemoWallet):
-
-    def __init__(self, keyfile, gpgdir="~/.gnupg", passphrase=None):
-        self.gpg = gnupg.GPG(gnupghome=gpgdir)
+    def __init__(self, keyfile, passphrase=None):
         self.keyfile = keyfile
-        f = open(self.keyfile, "rb")
-        pk = self.gpg.decrypt_file(f, passphrase=passphrase)
-        f.close()
-        logg.debug("decrypted pk with key {}".format(pk.key_id))
-        super(UsawaWallet, self).__init__(privatekey=pk.data)
+
+        with open(self.keyfile, "rb") as f:
+            data = f.read()
+
+        if passphrase is not None:
+            # Extract salt and decrypt
+            salt = data[: argon2i.SALTBYTES]
+            encrypted = data[argon2i.SALTBYTES :]
+            key = argon2i.kdf(
+                SecretBox.KEY_SIZE,
+                passphrase.encode(),
+                salt,
+            )
+            box = SecretBox(key)
+            try:
+                seed = box.decrypt(encrypted)
+            except Exception:
+                raise ValueError("decryption failed: wrong passphrase?")
+        else:
+            seed = data
+
+        if len(seed) != 32:
+            raise ValueError("expected 32 bytes, got {}".format(len(seed)))
+
+        logg.debug("wallet unlocked from keyfile")
+        super(UsawaWallet, self).__init__(privatekey=seed)
