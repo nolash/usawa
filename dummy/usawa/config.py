d2e15f04631ae21d9075cf82e12ee0891475b904
lash <dev@holbrook.no>
2026-07-15 13:18:14 -0600

    Implement topic in config, usawacontext in ledger create tool

--- dummy/usawa/config.py
+++ dummy/usawa/config.py
@@ -6,10 +6,12 @@ __datadir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
 logger = logging.getLogger(__name__)
 
 
-def load_config(config_dir=None, schemas=[]):
+def load_config(config_dir=None, schemas=[], topic=None):
     cfg = confini.Config(__datadir, env_prefix="USAWA", override_dirs=config_dir or [])
     for v in schemas:
         cfg.add_schema_dir(v)
     cfg.censor("WALLET_KEY_PASSPHRASE")
     cfg.process()
+    if topic != None:
+        cfg.add(topic, 'LEDGER_TOPIC', exists_ok=True)
     return cfg
